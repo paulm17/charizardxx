6a449f90a629fee5685421999b725f09b73f837e
Paul Moss <paulmoss06@gmail.com>
2024-11-13 09:45:01 +0000

    build: another commit

--- packages/@charizardxx-tests/core/src/shared/it-has-extend.tsx
+++
@@ -1,9 +0,0 @@
-interface Options {
-  component: React.ComponentType<any>;
-}
-
-export function itHasExtend(options: Options, name = 'has static extend function') {
-  it(name, () => {
-    expect(typeof (options.component as any).extend).toBe('function');
-  });
-}
--- packages/@charizardxx-tests/core/src/shared/it-has-withProps.tsx
+++
@@ -1,9 +0,0 @@
-interface Options {
-  component: React.ComponentType<any>;
-}
-
-export function itHasWithProps(options: Options, name = 'has static withProps function') {
-  it(name, () => {
-    expect(typeof (options.component as any).withProps).toBe('function');
-  });
-}
